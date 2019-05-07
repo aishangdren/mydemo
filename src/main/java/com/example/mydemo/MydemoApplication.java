953ce575d31d56ed2c05c9b4778a91c71da01277
aishangdren <104598891@qq.com>
2019-05-07 16:42:04 +0800

    :white_check_mark: Adding tests.

--- src/main/java/com/example/mydemo/MydemoApplication.java
+++ src/main/java/com/example/mydemo/MydemoApplication.java
@@ -2,12 +2,19 @@ package com.example.mydemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MydemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MydemoApplication.class, args);
     }
 
+    @GetMapping
+    private String ok(){
+        return "ok";
+    }
 }
